8ba42dfdb71e618638b264b4836919d9435320a6
Ibidapo Ayomide <ibidapoayomide754@gmail.com>
2024-06-05 11:08:23 -0700

    Added the add multiple tasks

--- src/layout/main/components/AddListKeep.tsx
+++ src/layout/main/components/AddListKeep.tsx
@@ -1,6 +1,5 @@
 import { Plus, Square, X } from 'lucide-react'
 import Input from '../../../components/Input'
-import { BiCheckbox } from 'react-icons/bi'
 import Button from '../../../components/Button'
 import { AddListProps } from '../../../props/AddTasksProps'
 
@@ -11,12 +10,18 @@ function AddListKeep({ setIsTyping, listValue, setListValue, name, index, items,
       <div className={`grid grid-cols-[20px,1fr,20px] gap-2 items-center px-4 ${index === isActiveList && "border-y"}`} onClick={() => {
         setIsActiveList(index)
         setIsTyping(false)
-      }}>
+      }}
+
+      >
+
+
         {isActiveList === index || items[index].text !== "" ? (
           <Square className="text-3xl text-secondary-text cursor-pointer" />
         ) : (
           <Plus className='w-4 text-secondary-text' />
         )}
+
+
         <Input style="listInput" variant="ghost" type='text' name={name} value={listValue} onChange={(e) => {
           setListValue((prev) => {
             const updated = [...prev]
@@ -25,28 +30,36 @@ function AddListKeep({ setIsTyping, listValue, setListValue, name, index, items,
           })
         }} placeholder='List Item' onInput={() => {
           setIsTyping(true)
+          if(index === items.length -1){
+            setListValue((prev) => [...prev, {
+              id: items.length + 1,
+              text: ""
+            }])
+          }
         }}
+
           onKeyPress={(e) => {
             if (e.key === "Enter") {
-              const lastItems = items[items.length - 1]
-              if (lastItems.text !== undefined) {
-                setListValue((prev) => [...prev, {
-                  id: items.length + 1,
-                  text: ""
-                }])
-              }
-              
               setIsTyping(false)
+              setIsActiveList(items.length -1)
             }
           }}
 
         />
-        {isActiveList === index  && (
+        {isActiveList === index && (
           <Button variant="ghost" size="icon3" className='p-1 w-7 h-7' onClick={() => {
-            if (items.length !== 1) {
-              const deleteItem = items.filter((item) => item.id !== id)
-              setListValue(deleteItem)
-            }
+           if(items.length <= 1){
+            setListValue([{
+              id: 0,
+              text: ""
+            }])
+            return
+           }
+
+           if (items.length > 1) {
+            const deleteItem = items.filter((item) => item.id !== id)
+            setListValue(deleteItem)
+          }
           }}>
             <X className='w-4 text-secondary-text' />
           </Button>
